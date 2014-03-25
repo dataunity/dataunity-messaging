aa55b1bf2a358f60b3cfd3877efe06676a8bef91
Kev K <kev@dataunity.org>
2014-03-25 16:41:11 +0000

    Add simple job queue.

---
+++ jobs/job_queue.py
@@ -0,0 +1,16 @@
+import zmq
+import logging
+
+if __name__ == '__main__':
+    # ToDo: read end points from args
+    context = zmq.Context()
+    pull_socket = context.socket(zmq.PULL)
+    pull_socket.bind('tcp://127.0.0.1:5000')
+    push_socket = context.socket(zmq.PUSH)
+    push_socket.bind('tcp://127.0.0.1:5001')
+
+    zmq.device(zmq.FORWARDER, pull_socket, push_socket)
+
+    pull_socket.close()
+    push_socket.close()
+    context.term()
